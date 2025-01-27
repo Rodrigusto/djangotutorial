71b86991092a772952b7c4f5f58ce7343de650c9
rodrigusto <rodrigusto@gmail.com>
2025-01-27 10:34:07 -0300

    part 5 tests

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,8 @@ from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
-
 from .models import Choice, Question
 
 
@@ -12,21 +12,25 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
-"""def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})"""
-    
 class DetailView(generic.DetailView):
     model = Question
-    template_name = "polls/detail.html" # -> se utilizar o caminho/model_nome da view <app name>/<model name>_detail.html - no caso polls/question_detail.html você não precisa especificar o nome do template, porem em determinado momento a class generica da view, requisitou o template e deu erro, provavelmente o problema está no model, e a não utilização da tag .super()
+    template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
-"""def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})"""
 
 class ResultsView(generic.DetailView):
     model = Question
